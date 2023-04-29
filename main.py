6210671ad2b1b11ae457f887e2d1fa8a3b1c506c
iambstha <iambstha@gmail.com>
2023-04-29 21:36:18 +0545

    Upload on test branch

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def test_eight_components():
 
 
     text_box = driver.find_element(by=By.XPATH, value="/html/body/div[4]/div[3]/div[2]/div[1]/a")
-    text_box.click();
+    print(text_box)
 
-    flash_element = driver.find_element(by=By.XPATH, value="/h tml/body/div[3]/div[2]");
-    sale_element = flash_element.find_elements(by=By.CLASS_NAME, value="sale-title");
-    for i in sale_element:
-        print(i.text)
+    # flash_element = driver.find_element(by=By.XPATH, value="/h tml/body/div[3]/div[2]");
+    # sale_element = flash_element.find_elements(by=By.CLASS_NAME, value="sale-title");
+    # for i in sale_element:
+    #     print(i.text)
 
     driver.quit()
 
