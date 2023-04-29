af40f642b6bac4678f4584b9ba6daad24223f4f8
iambstha <iambstha@gmail.com>
2023-04-29 22:18:18 +0545

    Working version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -7,16 +8,14 @@ driver.get("https://www.daraz.com.np/")
 
 
 def test_eight_components():
-
-
     text_box = driver.find_element(by=By.XPATH, value="/html/body/div[4]/div[3]/div[2]/div[1]/a")
-    print(text_box)
+    text_box.click()
 
-    # flash_element = driver.find_element(by=By.XPATH, value="/h tml/body/div[3]/div[2]");
-    # sale_element = flash_element.find_elements(by=By.CLASS_NAME, value="sale-title");
-    # for i in sale_element:
-    #     print(i.text)
+    flash_element = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[2]")
+    sale_element = flash_element.find_elements(by=By.CLASS_NAME, value="sale-title")
+    for i in sale_element:
+        print(i.text)
 
-    driver.quit()
+    # driver.quit()
 
 test_eight_components()
